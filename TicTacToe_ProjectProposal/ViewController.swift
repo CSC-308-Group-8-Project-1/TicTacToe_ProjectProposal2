e408d89c7ea528a3f800f38fe6ff7fa5e584a472
Eli-Krouse <williamelijahkrouse@gmail.com>
2026-02-12 12:58:39 -0500

    Initial Commit

---
+++ TicTacToe_ProjectProposal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe_ProjectProposal
+//
+//  Created by Krouse, William E. on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
